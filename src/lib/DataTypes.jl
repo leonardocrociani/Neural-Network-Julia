25a93d4cd519b965050cf08dca002e43820bfb5c
outra <outra@DESKTOP-FIRQL1U>
2025-01-21 23:58:11 +0100

    Backprop + Robustness

--- src/lib/DataTypes.jl
+++ src/lib/DataTypes.jl
@@ -1,7 +1,6 @@
 module DataTypes
 
-export Value
-export Operation
+
 
 mutable struct Value{opType} <: Number  # Value type is a subtype of Number
     data::Float64   # the actual value stored 
@@ -9,18 +8,20 @@ mutable struct Value{opType} <: Number  # Value type is a subtype of Number
     op::opType      # used to track the operation from which the Value is created (definition, operation...)
 end
 
-struct Operation{FuncType, ArgType} # to track the type of operation (addition, subtraction, ...) (argtype: type of the operands)
+struct Operation{FuncType, ArgTypes} # to track the type of operation (addition, subtraction, ...) (argtype: type of the operands)
     op::FuncType
-    args::ArgType
+    args::ArgTypes
 end
 
 # now the constructors:
 # Value from definition:
 Value(x::Number) = Value(Float64(x), 0.0, nothing) # grad is 0.0 for now, op is nothing for a definition
 
+export Value
+export Operation
 
 # for printing the values in a nice way:
-import Base
+import Base.show
 Base.show(io::IO, v::Value) = print(io, "Value($(v.data))")
 
 # overriding the == comparison so that it returns true only if the 2 Value objects are actually the same (in memory)
@@ -39,7 +40,7 @@ function +(a::Value, b::Value)
 end
 
 # backprop function for the case in which the val parameter comes from an addition operation
-function backprop!(val::Value(Operation(FunType, ArgTypes))) where (FunType<:typeof(+))
+function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(+), ArgTypes}
 	# let val = a + b, our aim is to update:
 	# a.grad, b.grad
 	# we use the rule for the derivative of the sum:
@@ -48,3 +49,39 @@ function backprop!(val::Value(Operation(FunType, ArgTypes))) where (FunType<:typ
 end
 
 end
+
+# backprop! is an internal function that updates the gradients for the operands 
+# depending on the operation used to create the variable.
+
+# The user calls the `backward` function on the final resulting variable for which 
+# they want to calculate the derivative. This function handles the correct 
+# backpropagation for the operation that was used to create the variable.
+
+# We perform a topological sort using DFS. We start from 
+# the final variable and update the gradients for the operands of the operation 
+# that was used to create the variable. At the end, we want an array where each 
+# value is preceded by all the operands that were used to produce it.
+
+function backward(a::Value)
+	function build_topo(v::Value, visited=Value[], topo=Value[])
+		if !(v in visited)
+			push!(visited, v)
+			if v.op != nothing
+				for operand in v.op.args
+					if operand isa Value
+						build_topo(operand, visited, topo)
+					end
+				end
+			end
+			push!(topo, v)
+		end
+		return topo
+	end
+	topo = build_topo(a)
+	a.grad = 1
+	for node in reverse(topo)
+		backprop!(node)
+	end
+end
+## adding support for operation Value + number
+Base.promote_rule(::Type{<:Value}, ::Type{T}) where {T<:Number} = Value
--- src/main.jl
+++ src/main.jl
@@ -1,5 +1,9 @@
-include("lib/DataTypes.jl") # modules are uppercased :|
+# Ensure your module is included correctly
+include("./lib/DataTypes.jl")
+
+# Import the module to bring the `Value` and `Operation` types into scope
 using .DataTypes
+
 
 x = Value(4)
 y = Value(4)
@@ -12,5 +16,22 @@ println(x == y) # should be false
 z = x # reference
 println(x == z) # should be true
 
-a = x+y
-println(a, " => data: $(a.data), grad: $(a.grad), op: $(a.op)")
+a = x + y
+println(a, " => data: $(a.data), grad: $(a.grad), op: $(a.op)") # Should show addition operation
+
+# backpropagation testing
+
+x = Value(2)
+y = Value(3)
+
+a = Value(1)
+b = Value(4)
+
+w = x + y
+z = a + b
+
+c = w + z
+
+backward(c)
+
+println(x.grad, y.grad, a.grad, b.grad) # should be 1.0 1.0 1.0 1.0
