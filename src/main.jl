2e19ad7f812f6a96b85e97e8fa20c690e1eb7ae2
Giacomo Trapani <giacomo.trapani01@libero.it>
2025-01-23 16:29:40 +0100

    basic testing is done

--- src/main.jl
+++ src/main.jl
@@ -15,7 +15,7 @@ bias_layer2 = Tensor(ones(1, 5))
 layer1_output = relu(inputs * weights_layer1 + bias_layer1)
 layer2_output = layer1_output * weights_layer2 + bias_layer2
 
-loss = softmax_crossentropy(layer2_output, y_true)
+loss = softmax_crossentropy(layer2_output, y_true, grad=true)
 println(loss)
 # println(layer2_output.grad)
 backward(loss)
