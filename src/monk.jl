b01347f68943ea28cd4689a597c4ed781e1afba2
Giacomo Trapani <giacomo.trapani01@libero.it>
2025-01-27 18:41:37 +0100

    some testing

--- src/monk.jl
+++ src/monk.jl
@@ -19,18 +19,18 @@ using .Scores
 vector_train_files = ["./dataset/monks/monks-1.train", "./dataset/monks/monks-2.train", "./dataset/monks/monks-3.train"]
 vector_test_files = ["./dataset/monks/monks-1.test", "./dataset/monks/monks-2.test", "./dataset/monks/monks-3.test"]
 
-X_train, Y_train, X_test, Y_test = load_monks_data(vector_train_files[1], vector_test_files[1]) # il primo task: monks-1
+train_dataset = load_monks_data(vector_train_files[1])
+test_dataset = load_monks_data(vector_test_files[1])
 
-X_train = hcat(X_train...)
-X_train = convert(Matrix{Float64}, X_train')
 
-X_test = hcat(X_test...)
-X_test = convert(Matrix{Float64}, X_test')
+X_train = convert(Matrix{Int64}, reduce(hcat, [Float64.(row) for row in [data[2] for data in train_dataset]])')
+Y_train = [data[3] for data in train_dataset]
 
-Y_train = convert(Vector{Float64}, Y_train)
-Y_test = convert(Vector{Float64}, Y_test)
 
-weight_init = random_initializer(42)
+X_test = convert(Matrix{Int64}, reduce(hcat, [Float64.(row) for row in [data[2] for data in test_dataset]])')
+Y_test = [data[3] for data in test_dataset]
+
+weight_init = random_initializer()
 bias_init = zeros_initializer()
 
 activation_fns = [Activations.sigmoid(), Activations.sigmoid()]
@@ -45,11 +45,11 @@ grid_params = Dict(
 )
 
 grid_search(
-	[0.1, 0.3, 0.8],
-	[0.01, 0.5, 0.1],
-	[50, 100, 200],
-	[10, 50],
-	[(6, 50), (50, 2)],
+	[0.875, 0.85, 0.8],
+	[0.875, 0.85, 0.8],
+	[40, 10, 20],
+	[400, 200],
+	[(17, 50), (50, 2)],
 	weight_init,
 	bias_init,
 	activation_fns,
