f5229bc03cd2dd68a8b02407ff15e28716eafae8
Giacomo Trapani <giacomo.trapani01@libero.it>
2025-01-27 11:21:49 +0100

    dump

--- src/lib/DataLoader.jl
+++ src/lib/DataLoader.jl
@@ -1,122 +1,135 @@
 module DataLoader
 
-    using DelimitedFiles
+	using DelimitedFiles
 
-    export load_monks_data, load_cup_data, save_cup_results
+	export load_monks_data, load_cup_data, save_cup_results
 
-    function load_monks_data(train_file::String, test_file::String)
+	function onehot_encode(y_true::Vector{Int64}, num_classes::Int)
+		# Initialize a matrix of zeros with size (num_samples, num_classes)
+		num_samples = length(y_true)
+		y_onehot = zeros(Int, num_samples, num_classes)
 
-        X_train = []
-        Y_train = []
+		# Set the appropriate indices to 1
+		for (i, class_idx) in enumerate(y_true)
+				y_onehot[i, class_idx + 1] = 1  # +1 because Julia uses 1-based indexing
+		end
 
-        X_test = []
-        Y_test = []
+		return y_onehot
+	end
 
-        # training
-        # read line by line
-        open(train_file) do file
-            for line in eachline(file)
-                line = strip(line)
-                line = split(line, " ")
+	function load_monks_data(train_file::String, test_file::String)
 
-                if length(line) <= 1
-                    continue
-                end
+		X_train = []
+		Y_train = Int[]
 
-                pop!(line) # last is the identifier
-                
-                x = parse.(Int, line[2:end])
-                y = parse(Int, line[1])
-                push!(X_train, x)
-                push!(Y_train, y)
-            end
-        end
+		X_test = []
+		Y_test = Int[]
 
-        open(test_file) do file
-            for line in eachline(file)
-                line = strip(line)
-                line = split(line, " ")
+		# training
+		# read line by line
+		open(train_file) do file
+			for line in eachline(file)
+				line = strip(line)
+				line = split(line, " ")
 
-                if length(line) <= 1
-                    continue
-                end
+				if length(line) <= 1
+					continue
+				end
 
-                pop!(line) # last is the identifier
-                
-                x = parse.(Int, line[2:end])
-                y = parse(Int, line[1])
-                push!(X_test, x)
-                push!(Y_test, y)
-            end
-        end
+				pop!(line) # last is the identifier
+				
+				x = parse.(Int, line[2:end])
+				y = parse(Int, line[1])
+				push!(X_train, x)
+				push!(Y_train, y)
+			end
+		end
 
-        println("Loading data...")
+		open(test_file) do file
+			for line in eachline(file)
+				line = strip(line)
+				line = split(line, " ")
 
-        return X_train, Y_train, X_test, Y_test
-    end
+				if length(line) <= 1
+					continue
+				end
 
+				pop!(line) # last is the identifier
+				
+				x = parse.(Int, line[2:end])
+				y = parse(Int, line[1])
+				push!(X_test, x)
+				push!(Y_test, y)
+			end
+		end
 
-    function load_cup_data(filename::String; test_set::Bool=false) 
-        X = []
-        Y = []
+		println("Loading data...")
 
-        first_line = false
-
-        open(filename) do file
-            for line in eachline(file)
-
-                if !first_line
-                    first_line = true
-                    continue
-                end
-
-                line = strip(line)
-                line = split(line, ",")
-
-                if length(line) <= 1
-                    continue
-                end
-
-                if test_set
-                    x = parse.(Float64, line[2:end])
-                    push!(X, x)
-                    continue
-                end
-
-                # last 3 columns are the target
-                x = parse.(Float64, line[2:end-3])
-                y = parse.(Float64, line[end-2:end])
-
-                push!(X, x)
-                push!(Y, y)
-            end
-        end
-
-        if test_set
-            return X
-        end
-
-        return X, Y
-    end
+		return X_train, onehot_encode(Y_train, 2), X_test, onehot_encode(Y_test, 2)
+	end
 
 
-    function save_cup_results(Y_pred::Array{Array{Float64, 1}, 1})
-        @assert length(Y_pred) == 500
-        @assert all([length(y) == 3 for y in Y_pred])
+	function load_cup_data(filename::String; test_set::Bool=false) 
+		X = []
+		Y = []
 
-        ln1 = "# Giovanni Braccini, Leonardo Crociani, Giacomo Trapani\n"
-        ln1 = ln1 * "# JTeam\n" 
-        ln1 = ln1 * "# ML-CUP24 V1\n"
-        ln1 = ln1 * "# 29/01/2025\n"
+		first_line = false
 
-        for i in 1:length(Y_pred)
-            y = Y_pred[i]
-            ln1 = ln1 * string(i) * "," * string(y[1]) * "," * string(y[2]) * "," * string(y[3]) * "\n"
-        end
+		open(filename) do file
+			for line in eachline(file)
 
-        open("../outputs/ML_CUP_24_TS_results.csv", "w") do file
-            write(file, ln1)
-        end
-    end
+				if !first_line
+					first_line = true
+					continue
+				end
+
+				line = strip(line)
+				line = split(line, ",")
+
+				if length(line) <= 1
+					continue
+				end
+
+				if test_set
+					x = parse.(Float64, line[2:end])
+					push!(X, x)
+					continue
+				end
+
+				# last 3 columns are the target
+				x = parse.(Float64, line[2:end-3])
+				y = parse.(Float64, line[end-2:end])
+
+				push!(X, x)
+				push!(Y, y)
+			end
+		end
+
+		if test_set
+			return X
+		end
+
+		return X, Y
+	end
+
+
+	function save_cup_results(Y_pred::Array{Array{Float64, 1}, 1})
+		@assert length(Y_pred) == 500
+		@assert all([length(y) == 3 for y in Y_pred])
+
+		ln1 = "# Giovanni Braccini, Leonardo Crociani, Giacomo Trapani\n"
+		ln1 = ln1 * "# JTeam\n" 
+		ln1 = ln1 * "# ML-CUP24 V1\n"
+		ln1 = ln1 * "# 29/01/2025\n"
+
+		for i in 1:length(Y_pred)
+			y = Y_pred[i]
+			ln1 = ln1 * string(i) * "," * string(y[1]) * "," * string(y[2]) * "," * string(y[3]) * "\n"
+		end
+
+		open("../outputs/ML_CUP_24_TS_results.csv", "w") do file
+			write(file, ln1)
+		end
+	end
 
 end
--- src/monk.jl
+++ src/monk.jl
@@ -16,8 +16,8 @@ using .NeuralNetworks
 using .HyperparamSearch
 using .DataLoader
 
-vector_train_files = ["../dataset/monks/monks-1.train", "../dataset/monks/monks-2.train", "../dataset/monks/monks-3.train"]
-vector_test_files = ["../dataset/monks/monks-1.test", "../dataset/monks/monks-2.test", "../dataset/monks/monks-3.test"]
+vector_train_files = ["./dataset/monks/monks-1.train", "./dataset/monks/monks-2.train", "./dataset/monks/monks-3.train"]
+vector_test_files = ["./dataset/monks/monks-1.test", "./dataset/monks/monks-2.test", "./dataset/monks/monks-3.test"]
 
 X_train, Y_train, X_test, Y_test = load_monks_data(vector_train_files[1], vector_test_files[1]) # il primo task: monks-1
 
@@ -27,48 +27,77 @@ X_train = convert(Matrix{Float64}, X_train')
 X_test = hcat(X_test...)
 X_test = convert(Matrix{Float64}, X_test')
 
-Y_train = convert(Vector{Int}, Y_train)
-Y_test = convert(Vector{Int}, Y_test)
-
 weight_init = random_initializer(42)
 bias_init = zeros_initializer()
 
 loss = mse()
 regularization = momentum_tikhonov()
 # Training the neural network with adjusted parameters
-nn = NeuralNetwork(
-    0.01,            # Increased learning rate
-    0.9,             # Increased momentum (α)
-    32,              # Larger batch size
-    200,             # Increased epochs for more learning
-    1,               # num_classes
-    [(6, 4), (4, 1)], # More complex network architecture
-    weight_init,
-    bias_init,
-    [Activations.tanh(), Activations.sigmoid()], # Use sigmoid for output layer
-    cross_entropy(), # Changed loss to cross-entropy for binary classification
-    regularization
+#nn = NeuralNetwork(
+#    0.01,            # Increased learning rate
+#    0.9,             # Increased momentum (α)
+#    32,              # Larger batch size
+#    200,             # Increased epochs for more learning
+#    1,               # num_classes
+#    [(6, 4), (4, 1)], # More complex network architecture
+#    weight_init,
+#    bias_init,
+#    [Activations.tanh(), Activations.sigmoid()], # Use sigmoid for output layer
+#    cross_entropy(), # Changed loss to cross-entropy for binary classification
+#    regularization
+#)
+
+grid_params = Dict(
+	:η => [0.01, 0.05, 0.1],
+	:α => [0.9, 0.01, 0.05],
+	:batch_sz => [32, 100, 10],
+	:epochs => [50, 200]
 )
 
-# Training the neural network
-train(nn, X_train, Y_train, verbose=true)
+keys_order = (:η, :α, :batch_sz, :epochs)
+combinations = Iterators.product([grid_params[k] for k in keys_order]...)
 
-correct = 0
-total = 0
-for i in eachindex(Y_test)
-    X_in = X_test[i:i,:]
-    X_in = Tensor(X_in)
-    Y_true = Y_test[i]
-    layer = nn.activation_functions[1](X_in * nn.layers[1] + nn.biases[1])
-    for i in 2:size(nn.layers, 1)
-        layer = nn.activation_functions[i](layer * nn.layers[i] + nn.biases[i])
-    end
-    prediction = (layer.data[1] >= 0.5) ? 1 : 0
-    println("output ", layer.data[1], " ", prediction, " ", Y_true)
-    if prediction == Y_true # -1 because digits start at 0
-        global correct +=1
-    end
-    global total += 1
+# Iterate over each combination
+for combo in combinations
+	# Extract the values for each parameter
+	η, α, batch_sz, epochs = combo
+	
+	println("η: $η, α: $α, batch_sz: $batch_sz, epochs: $epochs")
+
+		nn_prototype = NeuralNetwork(
+		η,
+		α,
+		batch_sz,
+		epochs,
+		1, # num_classes
+		[(6, 100), (100, 1)],
+		weight_init,
+		bias_init,
+		[Activations.tanh(), Activations.sigmoid()],
+		loss,
+		regularization
+		)
+
+		train(nn_prototype, X_train, Y_train)
+
+		correct = 0
+		total = 0
+		for i in axes(Y_test, 1)
+				X_in = X_test[i:i,:]
+				X_in = Tensor(X_in)
+				Y_true = Y_test[i]
+
+				layer = nn_prototype.activation_functions[1](X_in * nn_prototype.layers[1] + nn_prototype.biases[1])
+				for i in 2:size(nn_prototype.layers, 1)
+						layer = nn_prototype.activation_functions[i](layer * nn_prototype.layers[i] + nn_prototype.biases[i])
+				end
+
+				if argmax(layer.data, dims=2)[1][2] == Y_true
+						correct +=1
+				end
+				total += 1
+
+		end
+
+		println("accuracy: $(correct/total)") 
 end
-
-println("accuracy: $(correct/total)")
